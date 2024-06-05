22ff290b726ae1ce640f7260f46bdafc1aa29af1
Wenhao Ji <predator.ray@gmail.com>
2024-06-04 17:36:38 -0700

    Implemented a simplest version which only shuffles and deals community & hand cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { EncodedDeck, Player, StandardCard, createPlayer, decodeStandardCard, encodeStandardCard, getStandard52Deck } from 'mental-poker-toolkit';
+
 import './App.css';
 
+import CardImage from './components/CardImage';
+
+const useAliceAndBob = () => {
+  const [alice, setAlice] = useState<Player>();
+  const [bob, setBob] = useState<Player>();
+  const [communityCardsEncrypted, setCommunityCardsEncrypted] = useState<bigint[]>();
+
+  const [hands, setHands] = useState<StandardCard[]>();
+  const [communityCards, setCommunityCards] = useState<Array<StandardCard | null>>([null, null, null, null, null]);
+
+  useEffect(() => {
+    const loadCommunityCards = (async () => {
+      const deck = getStandard52Deck();
+      const alice = await createPlayer({
+        cards: deck.length,
+        bits: 128,
+      });
+      setAlice(alice);
+
+      const bob = await createPlayer({
+        cards: deck.length,
+        publicKey: alice.publicKey,
+        bits: 128,
+      });
+      setBob(bob);
+
+      const doubleDecrypt = (doubleEncrypted: bigint, offset: number): StandardCard => {
+        const decrypted = alice!.getIndividualKey(offset).decrypt(bob!.getIndividualKey(offset).decrypt(doubleEncrypted));
+        return decodeStandardCard(Number(decrypted));
+      }
+
+      const deckEncoded = new EncodedDeck(
+        deck.map((card) => BigInt(encodeStandardCard(card)))
+      );
+      const encryptedWithKeyA = alice.encryptAndShuffle(deckEncoded);
+      const encryptedWithKeyAKeyB = bob.encryptAndShuffle(encryptedWithKeyA);
+      const encryptedWithIndividualKeyAKeyB = alice.decryptAndEncryptIndividually(
+        encryptedWithKeyAKeyB
+      );
+      const encryptedBothKeysIndividually = bob.decryptAndEncryptIndividually(
+        encryptedWithIndividualKeyAKeyB
+      );
+
+      setCommunityCardsEncrypted(encryptedBothKeysIndividually.cards.slice(0, 5));
+
+      const handsEncrypted = encryptedBothKeysIndividually.cards.slice(5, 7);
+      setHands([
+        doubleDecrypt(handsEncrypted[0], 5),
+        doubleDecrypt(handsEncrypted[1], 6),
+      ]);
+    });
+    if (!communityCardsEncrypted) {
+      loadCommunityCards();
+    }
+  }, [
+    communityCardsEncrypted,
+  ]);
+
+  const flipCard = (offset: number) => {
+    if (alice && bob && communityCardsEncrypted) {
+      const cardDecrypted = bob.getIndividualKey(offset).decrypt(alice.getIndividualKey(offset).decrypt(communityCardsEncrypted[offset]));
+      const newCommunityCards = [...communityCards];
+      newCommunityCards[offset] = decodeStandardCard(Number(cardDecrypted));
+      setCommunityCards(newCommunityCards);
+    }
+  };
+
+  return {
+    ready: Boolean(alice && bob),
+    hands,
+    communityCards,
+    flipCard,
+  };
+};
+
 function App() {
+  const {
+    ready,
+    hands,
+    communityCards,
+    flipCard,
+  } = useAliceAndBob();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="community-cards">
+        {
+          ready ? (
+            communityCards.map((card, i) => 
+              <CardImage key={i} card={card} onClick={() => flipCard(i)}/>
+            )
+          ) : (
+            <>Shuffling the deck ...</>
+          )
+        }
+      </div>
+      
+      <div className="hand-cards">
+          {
+            hands && hands.map((card, i) => 
+              <CardImage key={i} card={card}/>
+            )
+          }
+      </div>
     </div>
   );
 }
---
+++ src/components/CardImage.tsx
@@ -0,0 +1,27 @@
+import React, { ImgHTMLAttributes } from 'react';
+import { StandardCard } from 'mental-poker-toolkit';
+
+export type CardImageProps = ImgHTMLAttributes<HTMLImageElement> & {
+  card?: StandardCard | null;
+};
+
+export default function CardImage(props: CardImageProps) {
+  if (!props.card) {
+    const {
+      alt,
+      className,
+      ...otherAttributes
+    } = props;
+    return <img className={className ? `card ${className}` : 'card'} src="/cards/back.svg" alt={alt ?? 'Back'} {...otherAttributes}/>;;
+  }
+
+  const {
+    alt,
+    className,
+    card,
+    ...otherAttributes
+  } = props;
+  
+  const src = `/cards/${(card.suit.charAt(0) + card.rank).toLowerCase()}.svg`;
+  return <img className={className ? `card ${className}` : 'card'} src={src} alt={alt ?? (card.suit + card.rank)} {...otherAttributes}/>;
+}
