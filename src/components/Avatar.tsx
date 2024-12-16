5ee1b58d93d3ccf7abedd9f531373396d63fc02a
Wenhao Ji <predator.ray@gmail.com>
2024-12-15 21:38:40 -0800

    Use a placeholder image in case multiavatar is unavailable

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,4 +1,4 @@
-import React, {ImgHTMLAttributes} from 'react';
+import React, {ImgHTMLAttributes, useMemo, useState} from 'react';
 
 type AvatarProperties =
   Pick<ImgHTMLAttributes<HTMLImageElement>,
@@ -6,14 +6,24 @@ type AvatarProperties =
   highlight?: boolean;
 };
 
+export const PLACEHOLDER_SRC = `${process.env.PUBLIC_URL}/avatar-placeholder.svg`
+
 export default function Avatar(props: AvatarProperties) {
   const {
     alt,
+    src,
     ...otherAttributes
   } = props;
+
+  const [error, setError] = useState<boolean>(false);
+
+  const actualSrc = useMemo(() => {
+    return error ? PLACEHOLDER_SRC : src;
+  }, [error, src]);
+
   return (
     <div className={props.highlight ? 'avatar highlight' : 'avatar'}>
-      <img alt={props.alt ?? "Avatar"} {...otherAttributes}/>
+      <img alt={props.alt ?? "Avatar"} src={actualSrc} {...otherAttributes} onError={() => setError(true)}/>
     </div>
   );
 }
