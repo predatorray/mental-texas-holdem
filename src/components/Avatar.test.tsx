2a0d95b9f85d79ec0beb489243b2808f939550cd
Wenhao Ji <predator.ray@gmail.com>
2024-12-16 19:31:05 -0800

    Use a placeholder image in case multiavatar is unavailable (#9)

--- src/components/Avatar.test.tsx
+++ src/components/Avatar.test.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import Avatar from "./Avatar";
+import {fireEvent, render, screen} from '@testing-library/react';
+import Avatar, {PLACEHOLDER_SRC} from "./Avatar";
 
 test('rendering does not crash', () => {
   render(<Avatar />);
+});
+
+test('rendering on error', () => {
+  render(<Avatar data-testid="avatar" src='url1' />);
+  const img = screen.getByTestId('avatar');
+  expect(img.getAttribute('src')).toContain('url1');
+
+  fireEvent.error(img);
+
+  const placeholder = img.getAttribute('src');
+  expect(placeholder).toBe(PLACEHOLDER_SRC);
 });
