1ca7380e64d779143462d7dcaecef09491d3fc5b
Wenhao Ji <predator.ray@gmail.com>
2024-12-18 14:18:05 -0800

    Refined the index page

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from "./App";
+import {Metric} from "web-vitals";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,4 +17,12 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function sendToAnalytics(metric: Metric) {
+  const { id, name, value } = metric;
+  gtag('event', name, {
+    event_category: 'Web Vitals',
+    event_label: id,
+    value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+  });
+}
+reportWebVitals(sendToAnalytics);
