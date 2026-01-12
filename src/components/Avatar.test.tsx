db6ec3d68552853773bd3767d7fb57bd109b35ae
Wenhao Ji <predator.ray@gmail.com>
2026-01-11 21:17:44 -0800

    Used `MultiAvatar` JS library instead of URL to improve availability (#19)

--- src/components/Avatar.test.tsx
+++ src/components/Avatar.test.tsx
@@ -1,18 +1,7 @@
 import React from 'react';
-import {fireEvent, render, screen} from '@testing-library/react';
-import Avatar, {PLACEHOLDER_SRC} from "./Avatar";
+import {render} from '@testing-library/react';
+import Avatar from "./Avatar";
 
 test('rendering does not crash', () => {
   render(<Avatar />);
-});
-
-test('rendering on error', () => {
-  render(<Avatar data-testid="avatar" src='url1' />);
-  const img = screen.getByTestId('avatar');
-  expect(img.getAttribute('src')).toContain('url1');
-
-  fireEvent.error(img);
-
-  const placeholder = img.getAttribute('src');
-  expect(placeholder).toBe(PLACEHOLDER_SRC);
 });
--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,4 +1,4 @@
-import React, {ImgHTMLAttributes, useMemo, useState} from 'react';
+import React, {ImgHTMLAttributes} from 'react';
 
 type AvatarProperties =
   Pick<ImgHTMLAttributes<HTMLImageElement>,
@@ -6,24 +6,15 @@ type AvatarProperties =
   highlight?: boolean;
 };
 
-export const PLACEHOLDER_SRC = `${process.env.PUBLIC_URL}/avatar-placeholder.svg`
-
 export default function Avatar(props: AvatarProperties) {
   const {
     alt,
-    src,
     ...otherAttributes
   } = props;
 
-  const [error, setError] = useState<boolean>(false);
-
-  const actualSrc = useMemo(() => {
-    return error ? PLACEHOLDER_SRC : src;
-  }, [error, src]);
-
   return (
     <div className={props.highlight ? 'avatar highlight' : 'avatar'}>
-      <img alt={props.alt ?? "Avatar"} src={actualSrc} {...otherAttributes} onError={() => setError(true)}/>
+      <img alt={props.alt ?? "Avatar"} {...otherAttributes}/>
     </div>
   );
 }
