c9926b3e05ea8e9f7ba74871ae307326b1189b58
Wenhao Ji <predator.ray@gmail.com>
2026-01-11 21:08:33 -0800

    Used `MultiAvatar` JS library instead of URL to improve availability

--- src/components/PlayerAvatar.tsx
+++ src/components/PlayerAvatar.tsx
@@ -1,6 +1,12 @@
 import Avatar from "./Avatar";
 import React from "react";
 import DataTestIdAttributes from "../lib/types";
+import multiavatar from '@multiavatar/multiavatar';
+
+function generateAvatarForSrcAttribute(playerId: string) {
+  const svgCode = multiavatar(playerId);
+  return `data:image/svg+xml;utf8,${encodeURIComponent(svgCode)}`;
+}
 
 export default function PlayerAvatar(props: DataTestIdAttributes & {
   playerId: string;
@@ -11,22 +17,23 @@ export default function PlayerAvatar(props: DataTestIdAttributes & {
 } | {
   children: React.ReactNode;
 } | {})) {
+  const src = generateAvatarForSrcAttribute(props.playerId);
   if ('playerName' in props) {
     return <div className="player-avatar" title={props.title ?? props.playerName} data-testid={props['data-testid']}>
-      <Avatar highlight={props.highlight} src={`https://api.multiavatar.com/${props.playerId}.svg`}/>
+      <Avatar highlight={props.highlight} src={src}/>
       <div className="avatar-label">{props.playerName}</div>
     </div>;
   }
   if ('children' in props) {
     return <div className="player-avatar" title={props.title} data-testid={props['data-testid']}>
-      <Avatar highlight={props.highlight} src={`https://api.multiavatar.com/${props.playerId}.svg`}/>
+      <Avatar highlight={props.highlight} src={src}/>
       <div className="avatar-label">{props.children}</div>
     </div>;
   }
   return <Avatar
     title={props.title}
     highlight={props.highlight}
-    src={`https://api.multiavatar.com/${props.playerId}.svg`}
+    src={src}
     data-testid={props['data-testid']}
   />;
 }
